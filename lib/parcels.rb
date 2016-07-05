9e617a48504eeda92aa527c2bf09cc84c2b83ea5
Alvin Ng and Sky Rousse <alvin@>
2016-07-05 14:34:07 -0700

    updated cost method and cooresponding specs

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,22 +1,23 @@
 class Parcel
-  define_method(:initialize) do |length, width, weight, distance, speed|
+  define_method(:initialize) do |length, width, height, weight, distance, speed|
     @length = length.to_i
     @width  = width.to_i
     @weight = weight.to_i
     @distance = distance.to_i
     @speed = speed.to_i
+    @height = height.to_i
   end
   define_method(:volume) do
-    @volume = @length.*(@width)
+    @volume = @length.*(@width).*(@height)
   end
   define_method(:cost) do
     @shipping_total = 0
     @weight_cost = @weight.*(0.5)
-    if @volume.>=(0..20) do
-      @shipping_total=(5.+(@weight_cost))
+    if volume() >= 0
+      @shipping_total=(5.+(@weight_cost).+(@speed).+(@distance))
     end
-    if @volume.>=(21..50) do
-      @shipping_total=(15.+(@weight_cost))
+    if volume() > 29
+      @shipping_total=(15.+(@weight_cost).+(@speed).+(@distance))
     end
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,11 +3,11 @@ require('parcels')
 
 describe('#volume') do
   it('returns the product of the sides of the object') do
-    expect(Parcel.new(2,5,50).volume()).to(eq(10))
+    expect(Parcel.new(2,5,1,1,1,1).volume()).to(eq(10))
   end
   describe('#cost') do
     it('returns the cost to ship an object') do
-      expect(Parcel.new(2,5,10,0,0).cost()).to(eq(50))
+      expect(Parcel.new(2,5,3,1,1,1).cost()).to(eq(17.5))
     end
   end
 end
