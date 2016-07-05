e264a72f8d330ae2aee02a39cdf91742909156dd
Alvin Ng and Sky Rousse <alvin@>
2016-07-05 15:49:06 -0700

    add Parcel class definition

--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,13 +1,15 @@
 require('rspec')
 require('parcels')
 
-describe('#volume') do
-  it('returns the product of the sides of the object') do
-    expect(Parcel.new(2,5,1,1,1,1).volume()).to(eq(10))
-  end
-  describe('#cost') do
-    it('returns the cost to ship an object') do
-      expect(Parcel.new(2,5,3,1,1,1).cost()).to(eq(17.5))
+describe(Parcel) do
+  describe('#volume') do
+    it('returns the product of the sides of the object') do
+      expect(Parcel.new(2,5,1,1,1,1).volume()).to(eq(10))
+    end
+    describe('#cost') do
+      it('returns the cost to ship an object') do
+        expect(Parcel.new(2,5,3,1,1,1).cost()).to(eq(17.5))
+      end
     end
   end
 end
